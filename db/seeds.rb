483ec783247679c87584b2550b7fcfc26e3d4a7e
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-04-27 13:36:14 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create(assignments:[], period:"Computer Science")
+course = Course.find(1)
+
+30.times do
+    course.students.create(
+        name: Faker::TvShows::FamilyGuy.character,
+        email: Faker::Internet.email,
+        comment: "new student"
+    )   
+end
+
+students = Student.all
+
+students.each do |student|
+    student.assignments.create([
+        {
+        name: "JS",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 14),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }, {
+        name: "Ruby",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 21),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }
+    }, {
+        name: "HTML",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 2),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }
+    }, {
+        name: "CSS",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 5),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }
+    }, {
+        name: "Data Structure",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 21),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }
+    }, {
+        name: "Algorithms",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 21),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }
+    }, {
+        name: "Python",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 14),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }
+    }, {
+        name: "C++",
+        grade: 0,
+        due_date: Faker::Date.forward(days: 10),
+        date_submitted: nil,
+        resubmit: false,
+        comment: nil
+    }
+    ])
+end
+
+
